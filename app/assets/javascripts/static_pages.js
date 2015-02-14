bbf94080beafbf321df3579393f2da4d5053d289
Jimmy <jimmylorunning@gmail.com>
2015-02-14 01:15:56 -0500

    Prevent focus event for generated tweets using javascript

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+	$($(".generated-tweet")).focus(function(e) {
+		$(this).blur();
+	});
+});
+
+// cannot use e.preventDefault() because focus event is not cancelable (e.cancelable = false)
+// http://help.dottoro.com/ljwolcsp.php
